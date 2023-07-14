743051f4a84cc356bde8057dede2e16bcae65e40
João Merlugo <joaopaulomerlugo000@gmail.com>
2023-07-14 14:29:21 -0300

    Initial version of blog generated by generator-jhipster@8.0.0-beta.2

---
+++ src/main/webapp/app/shared/config/languages.ts
@@ -0,0 +1,8 @@
+const languages = () => ({
+  'pt-br': { name: 'Português (Brasil)' },
+  en: { name: 'English' },
+  fr: { name: 'Français' },
+  // jhipster-needle-i18n-language-key-pipe - JHipster will add/remove languages in this object
+});
+
+export default languages;
